418de8cf934c558694ef8d9f7f990eb41ef7a214
Alexandre GARCIA <agarcia@henix.fr>
2024-02-23 08:48:09 +0100

    update

--- model/pages/testScreenPage.ts
+++ model/pages/testScreenPage.ts
@@ -5,12 +5,73 @@ import { getUrl } from "../../src/managers/test-maker.common";
 
 
 
-export class TestScreenPage{
-    public async loginViaUserPool(operator:any,runInfo:any){
+export class TestScreenPage {
+    private readonly addInformationButton = pega.button.byText('Add Information')
+    private readonly addInformationPopup = pega.elementById({ id: 'modaldialog_hd_title' })
+    private readonly attachContentButton = pega.button.byText('Attach content')
+    private readonly attachContentPopup = pega.elementById({ id: 'modalContent' })
+    private readonly attachFileButton = pega.elementByCss(`input[title="Select file(s)"]`)
+    private readonly bookField = pega.autocompletionFieldByXPath('//label[text()="Book"]/following-sibling::div/input[@type="text"]')
+    private readonly addBookButton = pega.button.byText('Add')
+    private readonly booksTableDiv = pega.elementByDataTestId({ dataTestId: '202004070615060476197-layout' })
+    private readonly booksTable = pega.tableByTitle('Books')
+
+    public async loginViaUserPool(operator: any, runInfo: any) {
         await pega.visit(getUrl(runInfo.configuration.extra.env.name), runInfo);
-        await pega.elementById({id:'txtUserID'}).waitUntilElementExists();
+        await pega.elementById({ id: 'txtUserID' }).waitUntilElementExists();
         await pega.loginForm.login(operator.username, operator.password)
     }
+
+    public async createTestCase() {
+        await pega.leftPanelMenu.createNewCase(`TestCafe`);
+        await pega.assignmentTitle.shouldHaveText('TestScreen');
+    }
+
+    public async openAddInformationPopup() {
+        await this.addInformationButton.click();
+    }
+    public async assertAddInformationPopupVisible() {
+        await this.addInformationPopup.waitUntilElementExists();
+        await this.addInformationPopup.shouldBeVisible();
+    }
+    public async cancelPopup() {
+        await pega.elementById({ id: 'ModalButtonCancel' }).click();
+    }
+    public async openAttachContent() {
+        await this.attachContentButton.click();
+    }
+    public async assertAttachContentPopupVisible() {
+        await this.attachContentPopup.waitUntilElementExists();
+        await this.attachFileButton.shouldBeVisible();
+    }
+    public async addBook(filtre: string, autor: string) {
+        await this.bookField.filter(filtre)
+        await this.bookField.selectResultFromExternalSource('BooksList', autor)
+        await this.addBookButton.click()
+    }
+    public async bookTabVisible(){
+        await this.booksTableDiv.waitUntilElementExists();
+        await this.booksTableDiv.waitUntilVisibility();
+    }
+    public async verifyCellReadonlyText(row: any, cell: any, expected: string) {
+        await this.booksTable.row(row).cell(cell).hover()
+        await this.booksTable.row(row).cell(cell).shouldHaveReadonlyText(expected)
+    }
+    public async tableCheckbox(row: any, cell: any) {
+        await this.booksTable.row(row).cell(cell).checkbox().check();
+    }
+    public async uploadFile() {
+        //pour upload un fichier
+        // await I.setFilesToUpload(`[id="$PpyAttachmentPage$ppxAttachName"]`,  './src/files/sample.pdf');
+
+        // await pega.textInput('20170515091207074851281').shouldHaveValue('sample');
+        // await pega.buttonByTitle('Attach').click();
+
+        // await pega.button.save().click();
+
+        // await pega.elementByAttributes({attributeName:'title',attributeValue:'sample.pdf'}).shouldBeVisible();
+    }
+
 }
 
 export const testScreen = new TestScreenPage();
--- src/specs/sac-1-correction-spec.ts
+++ src/specs/sac-1-correction-spec.ts
@@ -5,11 +5,14 @@ import { pega } from 'pega-model';
 let operator: any;
 
 Feature(`Test Screen Page`)
+    .beforeEachScenario(async (I, runInfo:any) => {
+        operator = await runInfo.configuration.extra.operatorsManager.assignOperator('user');
+    })
+    .afterEachScenario(async(I,runInfo:any)=>{
+        await runInfo.configuration.extra.operatorsManager.unAssignOperator('user', operator.username);
+    })
     .Scenario(`SAC-1-1`)
     .Given(`I logged in as a user`, async (I, runInfo: any) => {
-
-        operator = await runInfo.configuration.extra.operatorsManager.assignOperator('user');
-
         await testScreen.loginViaUserPool(operator, runInfo);
     })
     .When(`I\`m creating a new case`, async () => {
@@ -17,88 +20,54 @@ Feature(`Test Screen Page`)
     })
     .Then(`I can see TestScreen page`, async (I, runInfo: any) => {
         await pega.assignmentTitle.shouldHaveText('TestScreen');
-
-        await runInfo.configuration.extra.operatorsManager.unAssignOperator('user', operator.username);
     })
 
 
 
     .Scenario('SAC-1-2')
     .Given('i`m on the testScreenPage', async (I, runInfo: any) => {
-        operator = await runInfo.configuration.extra.operatorsManager.assignOperator('user');
         await testScreen.loginViaUserPool(operator, runInfo);
-        await pega.leftPanelMenu.createNewCase(`TestCafe`);
-        await pega.assignmentTitle.shouldHaveText('TestScreen');
-
+        await testScreen.createTestCase();
     })
     .When('i`m clicking the "add information" button', async () => {
-        await pega.button.byText('Add Information').click();
-        // await pega.buttonByDataTestId('2020040704574204653718') //avec le test id
+        await testScreen.openAddInformationPopup();
     })
     .Then('I can see Fillinfo popup', async (I, runInfo: any) => {
         // await pega.elementById({id:'modalWrapper'}).shouldBeVisible();
-        await pega.elementById({ id: 'modaldialog_hd_title' }).waitUntilElementExists();
-        await pega.elementById({ id: 'modaldialog_hd_title' }).shouldBeVisible();
-        await pega.elementById({ id: 'ModalButtonCancel' }).click();
-        await runInfo.configuration.extra.operatorsManager.unAssignOperator('user', operator.username);
+        await testScreen.assertAddInformationPopupVisible();
+        await testScreen.cancelPopup()
+
     })
 
 
 
     .Scenario('SAC-1-3')
     .Given(' I`m on the TestScreenPage', async (I, runInfo: any) => {
-        operator = await runInfo.configuration.extra.operatorsManager.assignOperator('user');
         await testScreen.loginViaUserPool(operator, runInfo);
-        await pega.leftPanelMenu.createNewCase(`TestCafe`);
-        await pega.assignmentTitle.shouldHaveText('TestScreen');
+        await testScreen.createTestCase();
     })
     .When('I`m clicking the “Attach content“ button', async () => {
-        await pega.button.byText('Attach content').click();
+        await testScreen.openAttachContent();
     })
-    .Then('I can upload documents', async (I,runInfo:any) => {
-        await pega.elementById({id:'modalContent'}).waitUntilElementExists();
-        await pega.elementByCss(`input[title="Select file(s)"]`).shouldBeVisible();
-        await pega.elementById({ id: 'ModalButtonCancel' }).click();
+    .Then('I can upload documents', async (I, runInfo: any) => {
+        await testScreen.assertAttachContentPopupVisible();
+        await testScreen.cancelPopup();
+        await testScreen.uploadFile();
 
-        //pour upload un fichier
-        // await I.setFilesToUpload(`[id="$PpyAttachmentPage$ppxAttachName"]`,  './src/files/sample.pdf');
-
-        // await pega.textInput('20170515091207074851281').shouldHaveValue('sample');
-        // await pega.buttonByTitle('Attach').click();
-
-        // await pega.button.save().click();
-
-        // await pega.elementByAttributes({attributeName:'title',attributeValue:'sample.pdf'}).shouldBeVisible();
-
-
-        await runInfo.configuration.extra.operatorsManager.unAssignOperator('user', operator.username);
     })
-
-
-
 
     .Scenario('SAC-1-4')
-    .Given('I`m on the TestScreenPage', async (I,runInfo:any) => {
-        operator = await runInfo.configuration.extra.operatorsManager.assignOperator('user');
+    .Given('I`m on the TestScreenPage', async (I, runInfo: any) => {
         await testScreen.loginViaUserPool(operator, runInfo);
-        await pega.leftPanelMenu.createNewCase(`TestCafe`);
-        await pega.assignmentTitle.shouldHaveText('TestScreen');
-    })
-    .When('I`m selecting the book in the “Book“ field', async (I) => {
+        await testScreen.createTestCase();
 
-        await pega.autocompletionFieldByXPath('//label[text()="Book"]/following-sibling::div/input[@type="text"]').filter('hamlet')
-        await pega.autocompletionFieldByXPath('//label[text()="Book"]/following-sibling::div/input[@type="text"]').selectResultFromExternalSource('BooksList','William Shakespeare')
-        await pega.button.byText('Add').click();
-        
     })
-    .Then('I can see the information in the table below(name, author, year and is Selected checkbox).',async(I,runInfo:any)=>{
-        await pega.elementByDataTestId({dataTestId:'202004070615060476197-layout'}).waitUntilElementExists();
-        await pega.elementByDataTestId({dataTestId:'202004070615060476197-layout'}).waitUntilVisibility();
-
-        await pega.tableByTitle('Books').row(1).cell(1).hover()
-        await pega.tableByTitle('Books').row(1).cell(1).shouldHaveReadonlyText('William Shakespeare')
-        await pega.tableByTitle('Books').row(1).cell(2).hover()
-        await pega.tableByTitle('Books').row(1).cell(2).shouldHaveReadonlyText('Hamlet')
-        await pega.tableByTitle('Books').row(1).cell(4).checkbox().check();
-        await runInfo.configuration.extra.operatorsManager.unAssignOperator('user', operator.username);
+    .When('I`m selecting the book in the “Book“ field', async () => {
+        await testScreen.addBook('Hamlet', 'William Shakespeare')
+    })
+    .Then('I can see the information in the table below(name, author, year and is Selected checkbox).', async (I, runInfo: any) => {
+        await testScreen.bookTabVisible();
+        await testScreen.verifyCellReadonlyText(1, 1, 'William Shakespeare')
+        await testScreen.verifyCellReadonlyText(1, 2, 'Hamlet')
+        await testScreen.tableCheckbox(1, 4)
     })
